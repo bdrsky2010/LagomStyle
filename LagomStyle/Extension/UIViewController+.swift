f5bab6e217b725bc1860167864b6ef5d2f4727be
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-15 21:01:55 +0900

    UserDefaults 관련 메서드 UIViewController extension method로 추가

--- LagomStyle/Extension/UIViewController+.swift
+++ LagomStyle/Extension/UIViewController+.swift
@@ -7,9 +7,26 @@
 
 import UIKit
 
+// MARK: UIViewController extension Method: get identifier
 extension UIViewController {
     
     static var identifier: String {
         return String(describing: self)
+    }
+}
+
+// MARK: UIViewController extension Method: get, set, remove UserDefaults Data
+extension UIViewController {
+    
+    func getUserDefaults(forKey: String) -> Any? {
+        return UserDefaults.standard.object(forKey: forKey)
+    }
+    
+    func setUserDefaults<T>(value: T, forKey: String) {
+        UserDefaults.standard.set(value, forKey: forKey)
+    }
+    
+    func removeUsetDefaults(forKey: String) {
+        UserDefaults.standard.removeObject(forKey: forKey)
     }
 }

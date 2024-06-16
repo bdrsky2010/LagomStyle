b1b786b9b81338864877a42c7be28dac44d208a8
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-16 22:33:08 +0900

    프로필 이미지 및 닉네임 설정 뷰컨트롤러 구현 완료
    - 첫 설정 인지, 변경인지 분기 나눠서 네비게이션 바 아이템 구성 완료
    - 저장 버튼 혹은 완료 버튼 클릭 시 UserDefaults에 데이터 저장
     - 저장 버튼: 닉네임, 프로필 이미지 인덱스 저장
     - 완료 버튼: 온보딩 화면 확인 여부, 닉네임, 프로필 이미지 인덱스 저장
    
    - 저장 완료 시 root 뷰컨트롤러 Main 뷰컨트롤러로 변경

---
+++ LagomStyle/ViewController/ProfileSetupViewController.swift
@@ -0,0 +1,212 @@
+//
+//  ProfileNicknameSetupViewController.swift
+//  LagomStyle
+//
+//  Created by Minjae Kim on 6/16/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class ProfileSetupViewController: UIViewController, ConfigureViewProtocol {
+    
+    private let profileImageView = ProfileImageView(imageSelectType: .selected)
+    
+    private let nicknameTextField: UITextField = {
+        let textField = UITextField()
+        textField.attributedPlaceholder = NSAttributedString(
+            string: LagomStyle.phrase.profileSettingPlaceholder,
+            attributes: [NSAttributedString.Key.font: LagomStyle.Font.bold16,
+                         NSAttributedString.Key.foregroundColor: LagomStyle.Color.lagomLightGray])
+        textField.borderStyle = .none
+        return textField
+    }()
+    
+    private let textFieldUnderBar: UIView = {
+        let view = UIView()
+        view.backgroundColor = LagomStyle.Color.lagomLightGray
+        return view
+    }()
+    
+    private let warningLabel: UILabel = {
+        let label = UILabel()
+        label.font = LagomStyle.Font.regular13
+        label.textColor = LagomStyle.Color.lagomPrimaryColor
+        return label
+    }()
+    
+    private lazy var completeButton = PrimaryColorRoundedButton(title: LagomStyle.phrase.profileSettingComplete) { [weak self] in
+        guard let self, let text = nicknameTextField.text else { return }
+        
+        UserDefaultsHelper.setUserDefaults(value: true, forKey: LagomStyle.UserDefaultsKey.isOnboarding)
+        UserDefaultsHelper.setUserDefaults(value: text, forKey: LagomStyle.UserDefaultsKey.nickname)
+        UserDefaultsHelper.setUserDefaults(value: selectedImageIndex,
+                                           forKey: LagomStyle.UserDefaultsKey.profileImageIndex)
+        
+        let mainViewController = MainViewController()
+        changeRootViewController(rootViewController: mainViewController)
+    }
+    
+    private var isEnabled = false
+    private var selectedImageIndex = Int.random(in: 0...11)
+    var pfSetupType: LagomStyle.PFSetupOption?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureView()
+    }
+    
+    private func configureView() {
+        view.backgroundColor = LagomStyle.Color.lagomWhite
+        
+        configureNavigation()
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+        configureContent()
+        configureProfileImageView()
+        configureTextField()
+    }
+    
+    func configureNavigation() {
+        guard let setupType = pfSetupType else { return }
+        
+        navigationController?.navigationBar.isHidden = false
+        navigationItem.title = setupType.title
+        
+        if setupType == .edit {
+            let rightBarButtonItem = UIBarButtonItem(title: "저장",
+                                                     style: .plain,
+                                                     target: self,
+                                                     action: #selector(saveButtonClicked))
+            navigationItem.rightBarButtonItem = rightBarButtonItem
+        }
+        
+        configureNavigationBackButton()
+    }
+    
+    @objc
+    private func saveButtonClicked() {
+        guard let text = nicknameTextField.text else { return }
+        
+        UserDefaultsHelper.setUserDefaults(value: text, forKey: LagomStyle.UserDefaultsKey.nickname)
+        UserDefaultsHelper.setUserDefaults(value: selectedImageIndex,
+                                           forKey: LagomStyle.UserDefaultsKey.profileImageIndex)
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(profileImageView)
+        view.addSubview(nicknameTextField)
+        view.addSubview(textFieldUnderBar)
+        view.addSubview(warningLabel)
+        view.addSubview(completeButton)
+    }
+    
+    func configureLayout() {
+        
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(view.snp.width).multipliedBy(0.3)
+        }
+        
+        nicknameTextField.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(20)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(44)
+        }
+        
+        textFieldUnderBar.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextField.snp.bottom)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(1)
+        }
+        
+        warningLabel.snp.makeConstraints { make in
+            make.top.equalTo(textFieldUnderBar.snp.bottom).offset(12)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        completeButton.snp.makeConstraints { make in
+            make.top.equalTo(warningLabel.snp.bottom).offset(32)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(44)
+        }
+    }
+    
+    func configureUI() {
+        completeButton.isEnabled = isEnabled
+    }
+    
+    func configureContent() {
+        let image = LagomStyle.Image.profile(index: selectedImageIndex).imageName
+        profileImageView.configureContent(image: image)
+    }
+    
+    private func configureTextField() {
+        nicknameTextField.delegate = self
+    }
+    
+    private func configureProfileImageView() {
+        profileImageView.isUserInteractionEnabled = true
+        
+        let tapGestuer = UITapGestureRecognizer(target: self, action: #selector(profileImageTapped))
+        profileImageView.addGestureRecognizer(tapGestuer)
+    }
+    
+    @objc
+    private func profileImageTapped() {
+        let profileImageSetupViewController = ProfileImageSetupViewController()
+        profileImageSetupViewController.pfImageSetupType = .setup
+        profileImageSetupViewController.selectedImageIndex = selectedImageIndex
+        profileImageSetupViewController.pfImageSetupDelegate = self
+        
+        navigationController?.pushViewController(profileImageSetupViewController, animated: true)
+    }
+}
+
+extension ProfileSetupViewController: UITextFieldDelegate {
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        
+        defer {
+            completeButton.isEnabled = isEnabled
+        }
+        
+        for char in text {
+            let string = String(char)
+            
+            guard text.count >= 2, text.count < 10 else {
+                warningLabel.text = LagomStyle.phrase.numberOfCharacterX
+                isEnabled = false
+                return
+            }
+            
+            if let _ = string.range(of: "^[@#$%]*$", options: .regularExpression) {
+                warningLabel.text = LagomStyle.phrase.specialCharacterX
+                isEnabled = false
+                return
+            }
+            
+            if let _ = string.range(of: "^[0-9]*$", options: .regularExpression) {
+                warningLabel.text = LagomStyle.phrase.includeNumbers
+                isEnabled = false
+                return
+            }
+        }
+        isEnabled = true
+        warningLabel.text = LagomStyle.phrase.availableNickname
+    }
+}
+
+extension ProfileSetupViewController: PFImageSetupDelegate {
+    
+    func setupPFImage(selectedIndex: Int) {
+        let image = LagomStyle.Image.profile(index: selectedIndex).imageName
+        profileImageView.configureContent(image: image)
+        selectedImageIndex = selectedIndex
+    }
+}

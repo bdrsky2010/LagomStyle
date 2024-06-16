06c2d3ce0dac0d2c3c944ca9ce38e34febed4bf6
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-16 14:57:19 +0900

    캡슐 모양의 커스텀 버튼인 검색 필터링 버튼 컴포넌트 구현
    # isTapped: Bool
     - 탭이 눌렸는 지 판단하기 위한 저장 프로퍼티
     - 해당 프로퍼티를 통해 버튼의 UI를 바꿔줌
    
    # tapAction: ((_ sender: CapsuleTapActionButton) -> Void)?
     - Gesture에 들어가는Tap Action
     - 해당 탭 액션을 통해 위의 isTapped 프로퍼티의 값을 변경해주면서 UI 변경

---
+++ LagomStyle/Component/CapsuleTapActionLabel.swift
@@ -0,0 +1,100 @@
+//
+//  CapsuleTapActionLabel.swift
+//  LagomStyle
+//
+//  Created by Minjae Kim on 6/16/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class CapsuleTapActionButton: UIView, ConfigureViewProtocol {
+    
+    private let label = UILabel()
+    
+    private var isTapped = false
+    
+    var tapAction: ((_ sender: CapsuleTapActionButton) -> Void)?
+    
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    convenience init(title: String, tag: Int, tapAction: ((_ sender: CapsuleTapActionButton) -> Void)?) {
+        self.init()
+        self.tag = tag
+        self.tapAction = tapAction
+        
+        label.text = title
+        
+        clipsToBounds = true
+        
+        isUserInteractionEnabled = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tappedAction))
+        addGestureRecognizer(tapGesture)
+        
+        configureView()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.height / 2
+    }
+    
+    @objc private func tappedAction() {
+        tapAction?(self)
+    }
+    
+    private func configureView() {
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+    }
+    
+    func configureHierarchy() {
+        addSubview(label)
+    }
+    
+    func configureLayout() {
+        label.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(8)
+        }
+    }
+    
+    func configureUI() {
+        layer.borderColor = LagomStyle.Color.lagomLightGray.cgColor
+        
+        if tag == 0 {
+            selectUI()
+        }
+    }
+    
+    func resultFilteringButtonTapped() {
+        isTapped.toggle()
+        
+        if isTapped {
+            selectUI()
+        } else {
+            unSelectUI()
+        }
+    }
+    
+    private func selectUI() {
+        label.textColor = LagomStyle.Color.lagomWhite
+        backgroundColor = LagomStyle.Color.lagomDarkGray
+        layer.borderWidth = 0
+    }
+    
+    private func unSelectUI() {
+        label.textColor = LagomStyle.Color.lagomBlack
+        backgroundColor = LagomStyle.Color.lagomWhite
+        layer.borderWidth = 1
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

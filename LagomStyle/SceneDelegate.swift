40122f40673552001078cf9e181ac0b3ff8093b4
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-16 22:14:41 +0900

    UserDefaults 값을 검사하여 온보딩 화면 보여줄 것인지 판단

--- LagomStyle/SceneDelegate.swift
+++ LagomStyle/SceneDelegate.swift
@@ -13,12 +13,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        
+        var navigationController: UINavigationController
+        
+        if let isOnboarding = UserDefaultsHelper.getUserDefaults(forKey: LagomStyle.UserDefaultsKey.isOnboarding) as? Bool, isOnboarding {
+            navigationController = UINavigationController(rootViewController: MainViewController())
+        } else {
+            navigationController = UINavigationController(rootViewController: OnboardingViewController())
+        }
+        
+        navigationController.navigationBar.tintColor = LagomStyle.Color.lagomBlack
+        navigationController.configureNavigationBarTitleFont(font: LagomStyle.Font.bold16,
+                                                             textColor: LagomStyle.Color.lagomBlack)
+        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 

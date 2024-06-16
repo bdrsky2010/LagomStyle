489d034ecb90502538aa88f1fe59ecf6ddc39891
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-16 14:58:11 +0900

    공통적으로 사용되는 캡슐 모양의 primary 색상을 갖고있는 버튼 컴포넌트

--- LagomStyle/Component/PrimaryColorRoundedButton.swift
+++ LagomStyle/Component/PrimaryColorRoundedButton.swift
@@ -6,3 +6,27 @@
 //
 
 import UIKit
+
+import SnapKit
+
+final class PrimaryColorRoundedButton: UIButton, ConfigureViewProtocol {
+    
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    convenience init(title: String) {
+        self.init()
+        configuration = .filled()
+        configuration?.cornerStyle = .capsule
+        configuration?.attributedTitle = AttributedString(
+            NSAttributedString(string: title, attributes: [NSAttributedString.Key.font: LagomStyle.Font.bold14,
+                                                           NSAttributedString.Key.foregroundColor: LagomStyle.Color.lagomWhite]))
+        configuration?.baseBackgroundColor = LagomStyle.Color.lagomPrimaryColor
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

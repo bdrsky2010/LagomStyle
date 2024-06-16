1f0c51952fed4cdfec0e4418780fa93998a02e8b
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-17 01:41:41 +0900

    IQKeyboardManagerSwift 패키지 추가

--- LagomStyle/AppDelegate.swift
+++ LagomStyle/AppDelegate.swift
@@ -7,15 +7,17 @@
 
 import UIKit
 
+import IQKeyboardManagerSwift
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         sleep(2) // LaunchScreen을 통한 스플래시 화면 2초간 유지
+        
+        IQKeyboardManager.shared.enable = true
         
         return true
     }

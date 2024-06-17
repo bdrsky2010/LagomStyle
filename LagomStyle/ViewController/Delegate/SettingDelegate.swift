148acad8618d74196d1f8e033535604fe5eff7fe
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-17 23:47:27 +0900

    세팅뷰에서 프로필을 편집했을 때 데이터 역전달 용도 델리게이트

---
+++ LagomStyle/ViewController/Delegate/SettingDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SettingDelegate.swift
+//  LagomStyle
+//
+//  Created by Minjae Kim on 6/17/24.
+//
+
+import Foundation
+
+protocol SettingDelegate {
+    func setupProfile(profileImageIndex: Int, nickname: String)
+}

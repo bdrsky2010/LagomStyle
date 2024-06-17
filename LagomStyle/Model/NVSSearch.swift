7683bf6a228579a78c2b27feb40cc908713ac24f
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-17 19:40:32 +0900

    배열의 contains 메서드에서 where을 사용하지 않고 좀 더 편하게 값을 비교하기 위해 Equatable 프로토콜을 채택

--- LagomStyle/Model/NVSSearch.swift
+++ LagomStyle/Model/NVSSearch.swift
@@ -16,21 +16,25 @@ struct NVSSearch: Codable {
 }
 
 // MARK: NAVER Shopping Product Data Model
-struct NVSProduct: Codable {
+struct NVSProduct: Codable, Equatable {
     var title: String
     let link: String
     let image: String
     let lprice: String
     let mallName: String
     let productId: String
+    
+    static func == (lhs: NVSProduct, rhs: NVSProduct) -> Bool {
+        return lhs.productId == rhs.productId
+    }
 }
 
 // MARK: NAVER Shopping Search Sort Options
 enum NVSSSort: CaseIterable {
     case sim
     case date
-    case asc
     case dsc
+    case asc
     
     var parameter: String {
         return String(describing: self)
@@ -42,9 +46,9 @@ enum NVSSSort: CaseIterable {
             return "정확도"
         case .date:
             return "날짜순"
-        case .asc:
-            return "가격높은순"
         case .dsc:
+            return "가격높은순"
+        case .asc:
             return "가격낮은순"
         }
     }

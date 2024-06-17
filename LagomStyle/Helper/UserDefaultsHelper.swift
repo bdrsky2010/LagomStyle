0760453c2fd78596ca1b8ced3a396a9d0fd93f83
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-17 16:15:31 +0900

    UserDefaults -> method에서 propertyWrapper로 변경
    # 변경한 이유
     - 커스텀 타입을 UserDefaults에 저장하고 싶어서 변경하게됐음
     - 제네릭 타입으로 getter, setter 메서드에서 디코딩, 인코딩 해야하기 때문

--- LagomStyle/Helper/UserDefaultsHelper.swift
+++ LagomStyle/Helper/UserDefaultsHelper.swift
@@ -10,6 +10,21 @@ import Foundation
 // MARK: get, set, remove UserDefaults Data
 enum UserDefaultsHelper {
     
+    @UserDefaultWrapper(key: LagomStyle.UserDefaultsKey.isOnboarding)
+    static var isOnboarding: Bool?
+    
+    @UserDefaultWrapper(key: LagomStyle.UserDefaultsKey.nickname)
+    static var nickname: String?
+    
+    @UserDefaultWrapper(key: LagomStyle.UserDefaultsKey.profileImageIndex)
+    static var profileImageIndex: Int?
+    
+    @UserDefaultWrapper(key: LagomStyle.UserDefaultsKey.recentSearchQueries)
+    static var recentSearchQueries: [String]?
+    
+    @UserDefaultWrapper(key: LagomStyle.UserDefaultsKey.likeProducts)
+    static var likeProducts: [NVSProduct]?
+    
     static func getUserDefaults(forKey: String) -> Any? {
         return UserDefaults.standard.object(forKey: forKey)
     }
@@ -20,5 +35,31 @@ enum UserDefaultsHelper {
     
     static func removeUsetDefaults(forKey: String) {
         UserDefaults.standard.removeObject(forKey: forKey)
+    }
+}
+
+@propertyWrapper
+struct UserDefaultWrapper<T: Codable> {
+    private let key: String
+    private let userDefaults = UserDefaults.standard
+
+    init(key: String) {
+        self.key = key
+    }
+    
+    var wrappedValue: T? {
+        get {
+            if let data = userDefaults.object(forKey: key) as? Data {
+                if let value = try? JSONDecoder().decode(T.self, from: data) {
+                    return value
+                }
+            }
+            return nil
+        }
+        set {
+            if let encoded = try? JSONEncoder().encode(newValue) {
+                userDefaults.setValue(encoded, forKey: key)
+            }
+        }
     }
 }

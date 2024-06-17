955c638a50969302e2f628549c133973c20b88a7
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-17 19:35:27 +0900

    테이블뷰에서 최근 검색어 삭제를 위한 델리게이트

---
+++ LagomStyle/ViewController/Delegate/RecentSearchQueryDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  RecentSearchQueryDelegate.swift
+//  LagomStyle
+//
+//  Created by Minjae Kim on 6/17/24.
+//
+
+import Foundation
+
+protocol RecentSearchQueryDelegate {
+    func removeQuery(row: Int)
+}

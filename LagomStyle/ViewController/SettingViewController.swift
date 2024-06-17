bcc3c5dcd0c78acf3ee139a749144e3c71adeee3
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-17 23:50:21 +0900

    세팅 뷰 구성중
    # 완료
     - 프로필 셀 탭 할 경우 -> 프로필 편집뷰로 푸시
     - 프로필 편집 후 다시 세팅뷰로 돌아올 경우 편집된 데이터 적용 완료
     - 탈퇴 셀 탭 할 경우 -> 탈퇴 알럿 출력
     - 탈퇴한 경우 root 뷰컨트롤러를 온보딩 뷰로 변경
     - 그리고 모든 UserDefaults 데이터 삭제

--- LagomStyle/ViewController/SettingViewController.swift
+++ LagomStyle/ViewController/SettingViewController.swift
@@ -5,4 +5,104 @@
 //  Created by Minjae Kim on 6/16/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class SettingViewController: UIViewController, ConfigureViewProtocol {
+    
+    private let settingTableView = UITableView()
+    
+    override func viewDidLoad() {
+        
+        configureView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        settingTableView.reloadData()
+    }
+    
+    private func configureView() {
+        view.backgroundColor = LagomStyle.Color.lagomWhite
+        
+        configureNavigation()
+        configureHierarchy()
+        configureLayout()
+        configureTableView()
+    }
+    
+    func configureNavigation() {
+        navigationItem.title = LagomStyle.phrase.settingViewNavigationTitle
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(settingTableView)
+    }
+    
+    func configureLayout() {
+        settingTableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func configureTableView() {
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
+        settingTableView.separatorStyle = .none
+        settingTableView.register(SettingTableViewProfileCell.self, forCellReuseIdentifier: SettingTableViewProfileCell.identifier)
+        settingTableView.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.identifier)
+    }
+}
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let index = indexPath.row
+        
+        if index == 0 {
+            let profileSetupViewController = ProfileSetupViewController()
+            profileSetupViewController.pfSetupType = .edit
+            navigationController?.pushViewController(profileSetupViewController, animated: true)
+        }
+        if index == 5 {
+            presentAlert(title: LagomStyle.phrase.withDrawAlertTitle,
+                         message: LagomStyle.phrase.withDrawAlertMessage) { [weak self] in
+                guard let self else { return }
+                UserDefaultsHelper.removeAllUserDefaults()
+                
+                let onboardingViewController = OnboardingViewController()
+                changeRootViewController(rootViewController: onboardingViewController)
+            }
+        }
+        
+        settingTableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return LagomStyle.phrase.settingOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.row == 0 ? 120 : 44
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let index = indexPath.row
+        
+        if index == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewProfileCell.identifier, for: indexPath) as? SettingTableViewProfileCell else { return UITableViewCell() }
+            cell.configureContent()
+            return cell
+        }
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath) as? SettingTableViewCell else { return UITableViewCell() }
+        if index == 1, let likeProducts = UserDefaultsHelper.likeProducts {
+            cell.configureContent(option: LagomStyle.phrase.settingOptions[index], likeProductsCount: likeProducts.count)
+        } else {
+            cell.configureContent(option: LagomStyle.phrase.settingOptions[index])
+        }
+        return cell
+    }
+    
+    
+}

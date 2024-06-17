d959628cd0225d5711da5e866b83374a52d58267
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-17 20:16:55 +0900

    웹뷰 연결 및 rightBarButtonItem을 눌렀을 때 델리게이트를 이용하여 Super뷰의 UI를 업데이트 해주면서 UserDefaults에 데이터 저장

--- LagomStyle/ViewController/NVSProductDetailViewController.swift
+++ LagomStyle/ViewController/NVSProductDetailViewController.swift
@@ -11,8 +11,60 @@ import WebKit
 import SnapKit
 
 final class NVSProductDetailViewController: UIViewController, ConfigureViewProtocol {
+    
+    private let webView = WKWebView()
+    
+    var delegate: NVSSearchDelegate?
+    var row: Int?
+    var isLike: Bool?
+    var productTitle: String?
+    var productLink: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+    }
+    
+    private func configureView() {
         view.backgroundColor = LagomStyle.Color.lagomWhite
+        
+        configureNavigation()
+        configureHierarchy()
+        configureLayout()
+        requestWebView()
+    }
+    
+    func configureNavigation() {
+        guard let productTitle, let isLike else { return }
+        navigationItem.title = productTitle
+        let likeBarButtonItem = UIBarButtonItem(image: UIImage(named: LagomStyle.Image.like(selected: isLike).imageName)?.withRenderingMode(.alwaysOriginal),
+                                                style: .plain,
+                                                target: self,
+                                                action: #selector(likeButtonClicked))
+        navigationItem.rightBarButtonItem = likeBarButtonItem
+    }
+    
+    @objc
+    private func likeButtonClicked() {
+        isLike?.toggle()
+        guard let isLike, let row else { return }
+        navigationItem.rightBarButtonItem?.image = UIImage(named: LagomStyle.Image.like(selected: isLike).imageName)?.withRenderingMode(.alwaysOriginal)
+        delegate?.setLikeButtonImageToggle(row: row, isLike: isLike)
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(webView)
+    }
+    
+    func configureLayout() {
+        webView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func requestWebView() {
+        guard let productLink, let url = URL(string: productLink) else { return }
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
 }

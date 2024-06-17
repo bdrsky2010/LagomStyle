0b2f66992b8c6f25b7245a8e67536bd116d0b6b2
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-17 19:35:03 +0900

    제품 디테일 뷰 구성 시작

---
+++ LagomStyle/ViewController/NVSProductDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NVSProductDetailViewController.swift
+//  LagomStyle
+//
+//  Created by Minjae Kim on 6/16/24.
+//
+
+import UIKit
+import WebKit
+
+import SnapKit
+
+final class NVSProductDetailViewController: UIViewController, ConfigureViewProtocol {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = LagomStyle.Color.lagomWhite
+    }
+}

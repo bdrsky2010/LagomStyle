e91660335a4b387e34ac795813fac06079fbe8ef
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-19 08:32:28 +0900

    계속 사용되는 메서드 프로토콜에 추가

--- LagomStyle/Protocol/ConfigureViewProtocol.swift
+++ LagomStyle/Protocol/ConfigureViewProtocol.swift
@@ -10,6 +10,7 @@ import Foundation
 // MARK: 공통으로 사용되는 View Configure 메서드 프로토콜
 @objc
 protocol ConfigureViewProtocol {
+    @objc optional func configureView()
     @objc optional func configureNavigation()
     @objc optional func configureHierarchy()
     @objc optional func configureLayout()

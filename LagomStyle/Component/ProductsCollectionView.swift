2f69354f8a178ee08e38b51e464704953f1fb5ae
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-30 17:20:34 +0900

    💾 Add: 스크린 사이즈 가져오는 타입 프로퍼티 추가로 인하여 코드 수정

--- LagomStyle/Component/ProductsCollectionView.swift
+++ LagomStyle/Component/ProductsCollectionView.swift
@@ -17,8 +17,8 @@ final class ProductsCollectionView: UICollectionView {
         let layout = UICollectionViewFlowLayout()
         let sectionSpacing: CGFloat = 16
         let cellSpacing: CGFloat = 16
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-            let screenWidth = windowScene.screen.bounds.width
+        
+        if let screenWidth = UIView.screenSize?.width {
             let itemWidth = screenWidth - (sectionSpacing * 2) - cellSpacing
             layout.itemSize = CGSize(width: itemWidth / 2, height: itemWidth / 1.2)
             layout.minimumLineSpacing = sectionSpacing

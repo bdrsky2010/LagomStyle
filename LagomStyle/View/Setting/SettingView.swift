bb50dc61ea53ecc14fbc5ca9666dc8f09e302a7d
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-30 11:22:56 +0900

    🚚 Move: 뷰 뷰컨 분리

--- LagomStyle/View/Setting/SettingView.swift
+++ LagomStyle/View/Setting/SettingView.swift
@@ -5,4 +5,24 @@
 //  Created by Minjae Kim on 6/25/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class SettingView: BaseView {
+    let settingTableView = UITableView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override func configureHierarchy() {
+        addSubview(settingTableView)
+    }
+    
+    override func configureLayout() {
+        settingTableView.snp.makeConstraints { make in
+            make.edges.equalTo(safeAreaLayoutGuide)
+        }
+    }
+}

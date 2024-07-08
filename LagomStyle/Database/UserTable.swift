22b2b349961a3473cfb9987433740fcd0ded3240
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-07-08 22:21:04 +0900

    💾 Add: 데이터베이스 테이블 추가

--- LagomStyle/Database/UserTable.swift
+++ LagomStyle/Database/UserTable.swift
@@ -9,11 +9,50 @@ import Foundation
 
 import RealmSwift
 
+class Note: EmbeddedObject {
+    @Persisted var contents: String
+    @Persisted var regDate: Date
+    @Persisted var updateDate: Date
+}
+
+class Folder: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var name: String
+    @Persisted var option: String
+    @Persisted var regDate: Date
+    
+    // 1:N to many relationship
+    @Persisted var detail: List<Basket>
+    
+    // 1:1 to one relationship
+    @Persisted var note: Note?
+}
+
+class Basket: Object {
+    @Persisted(primaryKey: true) var id: String
+    @Persisted(indexed: true) var name: String
+    @Persisted var mallName: String
+    @Persisted var lowPrice: String
+    @Persisted var webUrlString: String
+    @Persisted var imageUrlString: String
+    
+    convenience init(id: String, name: String, mallName: String, lowPrice: String, webUrlString: String, imageUrlString: String) {
+        self.init()
+        self.id = id
+        self.name = name
+        self.mallName = mallName
+        self.lowPrice = lowPrice
+        self.webUrlString = webUrlString
+        self.imageUrlString = imageUrlString
+    }
+}
+
 class UserTable: Object {
     @Persisted(primaryKey: true) var id: ObjectId
     @Persisted var nickname: String
     @Persisted var proflieImageIndex: Int
     @Persisted var signupDate: Date
+    @Persisted var recentSearchKeyword: Map<String, Date>
     
     convenience init(nickname: String, proflieImageIndex: Int, signupDate: Date) {
         self.init()

50bd65df81b64b30256d5d482b188ad9d4fb95d7
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-07-10 08:46:44 +0900

    🩹 Refactor: 클로저의 매개변수 추가 및 해당 클로저의 로직인 장바구니에 담아준 폴더가 변경됐을 경우 로직 구현
    장바구니에 담겼을 때 상품 디테일 화면쪽에서 장바구니에 담아준 상품이 전체 장바구니에 있는 지 있다면 원래 장바구니에 담겨 있던 데이터인지 새로 장바구니에 담긴 데이터인지 판단하여 상품 디테일 화면의 네비게이션 아이템 버튼의 이미지를 변경하고 이 뷰의 전 뷰 쪽으로 해당 변경사항에 대한 데이터를 클로저를 호출하여 전달

--- LagomStyle/ViewController/NVSProductDetailViewController.swift
+++ LagomStyle/ViewController/NVSProductDetailViewController.swift
@@ -11,6 +11,7 @@ import RealmSwift
 
 final class NVSProductDetailViewController: BaseViewController {
     private let nvsProductDetailView = NVSProductDetailView()
+    private let realmRepository = RealmRepository()
     
     var delegate: NVSSearchDelegate?
     var row: Int?
@@ -18,7 +19,7 @@ final class NVSProductDetailViewController: BaseViewController {
     var productID: String?
     var productTitle: String?
     var productLink: String?
-    var onChangeBasket: ((_ row: Int, _ isLike: Bool, _ folder: Folder) -> Void)?
+    var onChangeBasket: ((_ row: Int, _ isBasket: Bool, _ oldFolder: Folder?, _ newFolder: Folder) -> Void)?
     
     override func loadView() {
         view = nvsProductDetailView
@@ -34,9 +35,10 @@ final class NVSProductDetailViewController: BaseViewController {
     }
     
     override func configureNavigation() {
-        guard let productTitle, let isLike else { return }
+        guard let productTitle, let productID else { return }
         navigationItem.title = productTitle
-        let likeBarButtonItem = UIBarButtonItem(image: UIImage(named: LagomStyle.AssetImage.like(selected: isLike).imageName)?.withRenderingMode(.alwaysOriginal),
+        let isBasket = realmRepository.fetchItem(of: Basket.self).contains(where: { $0.id == productID })
+        let likeBarButtonItem = UIBarButtonItem(image: UIImage(named: LagomStyle.AssetImage.like(selected: isBasket).imageName)?.withRenderingMode(.alwaysOriginal),
                                                 style: .plain,
                                                 target: self,
                                                 action: #selector(likeButtonClicked))
@@ -45,15 +47,27 @@ final class NVSProductDetailViewController: BaseViewController {
     
     @objc
     private func likeButtonClicked() {
-        isLike?.toggle()
-        guard let isLike, let row else { return }
-        navigationItem.rightBarButtonItem?.image = UIImage(named: LagomStyle.AssetImage.like(selected: isLike).imageName)?.withRenderingMode(.alwaysOriginal)
-//        delegate?.setLikeButtonImageToggle(row: row, isLike: isLike)
+        guard let productID, let row else { return }
         let addOrMoveBasketFolderViewController = AddOrMoveBasketFolderViewController()
-        addOrMoveBasketFolderViewController.onChangeFolder = { [weak self] folder in
+        addOrMoveBasketFolderViewController.productID = productID
+        addOrMoveBasketFolderViewController.onChangeFolder = { [weak self] newFolder in
             guard let self else { return }
-            print(folder.name)
-            onChangeBasket?(row, isLike, folder)
+            var isBasket = false
+            var oldFolder: Folder?
+            let basketList = realmRepository.fetchItem(of: Basket.self)
+            for basket in basketList {
+                if basket.id == productID {
+                    isBasket = true
+                    oldFolder = basket.folder.first
+                    break
+                }
+            }
+            if let oldFolder {
+                navigationItem.rightBarButtonItem?.image = UIImage(named: LagomStyle.AssetImage.like(selected: oldFolder.id != newFolder.id).imageName)?.withRenderingMode(.alwaysOriginal)
+            } else {
+                navigationItem.rightBarButtonItem?.image = UIImage(named: LagomStyle.AssetImage.like(selected: true).imageName)?.withRenderingMode(.alwaysOriginal)
+            }
+            onChangeBasket?(row, isBasket, oldFolder, newFolder)
         }
         let navigationController = UINavigationController(rootViewController: addOrMoveBasketFolderViewController)
         present(navigationController, animated: true)

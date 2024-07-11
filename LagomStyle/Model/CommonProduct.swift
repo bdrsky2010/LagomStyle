de0c40beeb947a28661f26bf32a265751a441944
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-07-11 09:21:26 +0900

    🚚 Move: 따로 외부 폴더에 코드 이동

---
+++ LagomStyle/Model/CommonProduct.swift
@@ -0,0 +1,16 @@
+//
+//  CommonProduct.swift
+//  LagomStyle
+//
+//  Created by Minjae Kim on 7/11/24.
+//
+
+import Foundation
+
+// NVSProduct 타입과 Basket 타입을 연결해주는 공통 타입
+struct CommonProduct {
+    let title: String
+    let mallName: String
+    let lowPrice: String
+    let imageUrlString: String
+}

f1d48be23fdbd33a3c2e36f8d7f865f6a8e09628
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-07-15 11:13:43 +0900

    💾 Add: 다른 뷰에서도 사용하기 위해서 타입 변환을 용이하게 해주는 생성자를 구현

--- LagomStyle/Model/CommonProduct.swift
+++ LagomStyle/Model/CommonProduct.swift
@@ -9,8 +9,27 @@ import Foundation
 
 // NVSProduct 타입과 Basket 타입을 연결해주는 공통 타입
 struct CommonProduct {
+    let id: String
     let title: String
     let mallName: String
     let lowPrice: String
     let imageUrlString: String
+    let webUrlString: String
+    
+    init(id: String, title: String, mallName: String, lowPrice: String, imageUrlString: String, webUrlString: String) {
+        self.id = id
+        self.title = title
+        self.mallName = mallName
+        self.lowPrice = lowPrice
+        self.imageUrlString = imageUrlString
+        self.webUrlString = webUrlString
+    }
+    
+    init(contentsOf product: NVSProduct) {
+        self.init(id: product.productID, title: product.title, mallName: product.mallName, lowPrice: product.lowPrice, imageUrlString: product.imageUrlString, webUrlString: product.urlString)
+    }
+    
+    init(contentsOf basket: Basket) {
+        self.init(id: basket.id, title: basket.name, mallName: basket.mallName, lowPrice: basket.lowPrice, imageUrlString: basket.imageUrlString, webUrlString: basket.webUrlString)
+    }
 }
